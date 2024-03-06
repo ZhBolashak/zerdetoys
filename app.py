2dd5c5d0f004dcbb1ca26b80d251d387dd70fef4
ZhBolashak <160239279+ZhBolashak@users.noreply.github.com>
2024-03-07 01:45:33 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,22 @@
-from dash import Dash, dcc, html, Input, Output, State
+# app.py
+from dash import Dash, dcc, html
 import dash_bootstrap_components as dbc
-# Импортируем функции из файла table_product.py
-from table_product import update_table, update_dropdowns
+from frontend.layouts.layout_main import product_get_layout, sale_get_layout, download_excel_layout
+from frontend.callbacks.callback_product import register_callbacks as register_product_callbacks
+from frontend.callbacks.callback_sale import sale_callbacks as register_sale_callbacks
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-server = app.server  # Добавьте эту строку
 
-# Добавление невидимого компонента для инициализации данных
-app.layout = dbc.Container([
-    dbc.Row(dbc.Col(html.H1("Остаток товара"))),
-    dbc.Row([
-        dbc.Col(dcc.Dropdown(id='store-dropdown', multi=True, placeholder='Магазин')),
-        dbc.Col(dcc.Dropdown(id='provider-dropdown', multi=True, placeholder='Поставщик'))
-    ]),
-    dbc.Row(dbc.Col(html.Button('Поиск', id='search-button', n_clicks=0))),
-    dbc.Row(dbc.Col(html.Div(id='products-table'))),
-    # Невидимый компонент
-    html.Div(id='init', style={'display': 'none'})
+app.layout = html.Div([
+    dcc.Tabs([
+        dcc.Tab(label='Остаток товара', children=[product_get_layout(), download_excel_layout()]),
+        dcc.Tab(label='Движение средств', children=[sale_get_layout()]),
+    ])
 ])
 
-# Callback для обновления выпадающих списков
-@app.callback(
-    [Output('store-dropdown', 'options'),
-     Output('provider-dropdown', 'options')],
-    Input('init', 'children')
-)
-def dropdowns_callback(_):
-    return update_dropdowns(_)
-
-# Callback для обновления таблицы продуктов
-@app.callback(
-    Output('products-table', 'children'),
-    [Input('search-button', 'n_clicks')],
-    [State('store-dropdown', 'value'),
-     State('provider-dropdown', 'value')]
-)
-def table_callback(n_clicks, selected_stores, selected_providers):
-    return update_table(n_clicks, selected_stores, selected_providers)
+# Регистрация коллбэков для каждой вкладки
+register_product_callbacks(app)
+register_sale_callbacks(app)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
