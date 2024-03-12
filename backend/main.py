e87122fd1874c884d129a3f3d0b09b276b0b3f63
ZhBolashak <160239279+ZhBolashak@users.noreply.github.com>
2024-03-12 06:58:33 +0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,11 @@
 from fastapi import FastAPI
 from backend.product_endpoints import product_balance_router
 from backend.sale_endpoints import sales_router
+from backend.debt_endpoints import debt_router
 
 app = FastAPI()
 
+
 app.include_router(product_balance_router, prefix="/api/v1", tags=["Остаток товара"])
 app.include_router(sales_router, prefix="/api/dds", tags=["ДДС"])
+app.include_router(debt_router, prefix="/api/debt", tags=["Дебиторка"])
