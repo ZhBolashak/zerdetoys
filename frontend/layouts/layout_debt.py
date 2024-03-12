59a76bba10ae4fd227fd17e85fa8433e525ce350
ZhBolashak <160239279+ZhBolashak@users.noreply.github.com>
2024-03-12 06:56:49 +0500

    Add files via upload

---
+++ frontend/layouts/layout_debt.py
@@ -0,0 +1,21 @@
+#layout_debt.py
+from dash import  dcc, html
+import dash_bootstrap_components as dbc
+
+# Функция для получения лэйаута для страницы ДДС
+def debt_get_layout():
+    return dbc.Container([
+        dbc.Row(dbc.Col(html.H1("Дебиторка"))),
+        dbc.Row([
+            dbc.Col(dcc.Dropdown(id='client-dropdown', multi=False, placeholder='Выберите клиента')),
+            dbc.Col(dcc.DatePickerRange(
+                id='date-picker-range',
+                start_date_placeholder_text='Начальная дата',
+                end_date_placeholder_text='Конечная дата',
+                display_format='YYYY-MM-DD'
+            ))
+        ]),
+        dbc.Row(dbc.Col(dbc.Button('Получить отчет', id='get-debt-button', n_clicks=0, color='primary', className='me-1'))),
+        dbc.Row(dbc.Col(html.Div(id='report-debt-output'))),
+        html.Div(id='init_debt', style={'display': 'none'})
+    ])
